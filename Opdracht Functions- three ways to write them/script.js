0470b63d074e448ebd7cda865ad227339820273f
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-07-20 13:19:57 +0200

    Add files via upload

---
+++ Opdracht Functions- three ways to write them/script.js
@@ -0,0 +1,17 @@
+/*function add(number1, number2){
+    return number1 + number2;
+}
+console.log(add(5,6));
+
+/*const add = function (number1, number2){
+    return number1 + number2;
+};
+
+console.log(add(5,6));*/
+
+const add = (number1, number2) =>{
+return number1 + number2;
+};
+
+console.log (add(5,6));
+
